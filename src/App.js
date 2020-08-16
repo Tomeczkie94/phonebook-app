876ed10c41040bcf6bddb4c4debe627b34380aca
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-08-16 08:37:45 +0200

    Error - Unexpected use of 'name'  no-restricted-globals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { id: 'Arto Hellas',
+      name: 'Arto Hellas' }
   ])
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      id: persons.name,
+      name: newName,
+      // number:
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <Person key={person.id} name={name}/>)}
+
     </div>
   )
 }
